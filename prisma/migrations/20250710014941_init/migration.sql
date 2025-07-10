6bcd213f7e2f8efe30fc0fe5ca8b0651c3065eb5
mm <467057463@qq.com>
2025-07-10 10:08:05 +0800

    fix: deploy

--- prisma/migrations/20250710014941_init/migration.sql
+++ prisma/migrations/20250710014941_init/migration.sql
@@ -1,10 +1,19 @@
 -- CreateTable
+CREATE TABLE `WxToken` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `access_token` VARCHAR(191) NOT NULL,
+    `expires_in` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
+    `openid` VARCHAR(191) NOT NULL,
+    `session_key` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_openid_key`(`openid`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
