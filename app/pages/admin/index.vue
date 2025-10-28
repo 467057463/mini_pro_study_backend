bb6d06cbd6b83f0fe8c8ade75a0eb46069b449f1
mm <467057463@qq.com>
2025-10-28 20:44:21 +0800

    feat: 打印模版

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,19 +1,41 @@
 <template>
   <section>
     <el-card shadow="never">
-      admin index
+      admin index {{ c }} {{ b }}
     </el-card>
 
     <el-card shadow="never">
-      <button @click="counter++">+</button>
-      <button @click="counter++">-</button>
+      <button @click="handleTest">+</button>
+      <button @click="b += 33">-</button>
     </el-card>
   </section>
 </template>
 
-<script setup>
+<script setup lang="ts">
 definePageMeta({
   title: '首页',
   middleware: ['auth', 'role']
+})
+
+const a = ref(1)
+const b = ref(2)
+function add(a: Ref<number> | number, b: Ref<number> | number){
+  return computed(() => unref(a) + unref(b))
+}
+const c = add(a, 10)
+
+function handleTest(){
+  a.value = 10
+}
+
+unref(1)
+toValue(1)
+
+watchEffect(()=> {
+  console.log(a, b)
+  console.log('sssbbb')
+  // onCleanup(() => {
+  //   console.log('ssss')
+  // })
 })
 </script>
---
+++ app/pages/p.vue
@@ -0,0 +1,78 @@
+<template>
+  <Body class="p">
+    <div class="actions no-print">
+      <el-button @click="handlePrint">test</el-button>
+    </div>
+    <table>
+      <tbody>
+        <tr v-for="i in 2" :key="i">
+          <td v-for="a in 3" :key="a">{{ i }} - {{ a }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </Body>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "blank"
+})
+
+function handlePrint(){
+  window.print()
+}
+</script>
+
+<style lang="scss" scoped>
+.actions{
+  width: 250px;
+  height: 100%;
+  overflow: auto;
+}
+table{
+  width: 100%;
+  height: 100vh;
+  border-collapse: collapse;
+  line-height: 1;
+  // tr{
+  //   height: 33.3%;
+  // }
+  td {
+    border-left: 1px solid #ccc;
+    border-top: 1px solid #ccc;
+    align-content: flex-start;
+    padding: 10px;
+  }
+  tr:first-child td{
+    border-top: none;
+  }
+
+  td:first-child{
+    border-left: none;
+  }
+}
+
+@media print {
+  @page {
+    size: portrait;
+    // 去掉页眉页脚
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-left: 0;
+    margin-right: 0;
+  }
+  // 告诉浏览器在渲染它时不要对框进行颜色或样式调整
+  * {
+    -webkit-print-color-adjust: exact !important;
+    -moz-print-color-adjust: exact !important;
+    -ms-print-color-adjust: exact !important;
+    print-color-adjust: exact !important;
+  }
+  .no-print{
+    display: none;
+  }
+  table{
+    font-size: 9pt;
+  }
+}
+</style>
